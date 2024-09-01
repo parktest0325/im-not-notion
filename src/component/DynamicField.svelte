ccfda33dc423d36f82161b5d26655cd89e1d07b2
dongkim <parktest0325@gmail.com>
2024-09-01 15:49:31 +0900

    refactor: input field #19

---
+++ src/component/DynamicField.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    export let config: Record<string, string>;
+    export let configKey: string;
+    export let hidePassword: boolean = true;
+  
+    let isPasswordField = configKey.toLowerCase().includes("password");
+  
+    function toggleHidePassword() {
+      hidePassword = !hidePassword;
+    }
+  </script>
+  
+  <div class="flex items-center space-x-2">
+    <!-- svelte-ignore a11y-label-has-associated-control -->
+    <label class="block min-w-[120px]">{configKey}</label>
+    {#if isPasswordField && hidePassword}
+      <input
+        class="flex-1 p-2 border rounded"
+        type="password"
+        bind:value={config[configKey]}
+      />
+    {:else}
+      <input class="flex-1 p-2 border rounded" bind:value={config[configKey]} />
+    {/if}
+    {#if isPasswordField}
+      <button class="p-2 border rounded password-toggle" on:click={toggleHidePassword}>
+        {hidePassword ? "Show" : "Hide"}
+      </button>
+    {/if}
+  </div>
+  
+  <style>
+    .password-toggle {
+      background-color: var(--button-bg-color);
+      color: var(--button-text-color);
+      cursor: pointer;
+      transition: background-color 0.25s;
+    }
+  
+    .password-toggle:hover {
+      background-color: var(--button-hover-bg-color);
+    }
+  </style>
+  
--- src/sidebar/SettingsPopup.svelte
+++ src/sidebar/SettingsPopup.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api";
+  import DynamicField from "../component/DynamicField.svelte";
 
   export let show: boolean;
   export let closeSettings: () => void;
@@ -7,7 +8,6 @@
   let config: AppConfig;
   let isLoading = true; // 로딩 상태 추가
   let activeTab = "ssh"; // 'ssh' 또는 'hugo'가 될 수 있음
-  let hidePassword = true;
 
   $: if (show) {
     // show가 true일 때만 설정 로드
@@ -86,146 +86,15 @@
         <!-- 설정 입력 필드 -->
         {#if activeTab === "ssh"}
           <div class="space-y-4">
-            <!-- SSH 설정 필드 -->
-            <div class="flex items-center space-x-2">
-              <label class="block min-w-[120px]" for="ssh_client-host"
-                >Host</label
-              >
-              <input
-                class="flex-1 p-2 border rounded"
-                id="ssh_client-host"
-                bind:value={config.ssh_config.host}
-              />
-            </div>
-            <div class="flex items-center space-x-2">
-              <label class="block min-w-[120px]" for="ssh_client-port"
-                >Port</label
-              >
-              <input
-                class="flex-1 p-2 border rounded"
-                id="ssh_client-port"
-                bind:value={config.ssh_config.port}
-              />
-            </div>
-            <div class="flex items-center space-x-2">
-              <label class="block min-w-[120px]" for="ssh_client-username"
-                >Username</label
-              >
-              <input
-                class="flex-1 p-2 border rounded"
-                id="ssh_client-username"
-                bind:value={config.ssh_config.username}
-              />
-            </div>
-            <div class="flex items-center space-x-2">
-              <label class="block min-w-[120px]" for="ssh_client-password"
-                >Password</label
-              >
-              {#if hidePassword}
-                <input
-                  class="flex-1 p-2 border rounded"
-                  id="ssh_client-password"
-                  type="password"
-                  bind:value={config.ssh_config.password}
-                />
-              {:else}
-                <input
-                  class="flex-1 p-2 border rounded"
-                  id="ssh_client-password"
-                  bind:value={config.ssh_config.password}
-                />
-              {/if}
-              <button
-                class="p-2 border rounded password-toggle"
-                on:click={() => (hidePassword = !hidePassword)}
-              >
-                {hidePassword ? "Hide" : "Show"}
-              </button>
-            </div>
-            <div class="flex items-center space-x-2">
-              <label class="block min-w-[120px]" for="ssh_client-key_path"
-                >Key Path</label
-              >
-              <input
-                class="flex-1 p-2 border rounded"
-                id="ssh_client-key_path"
-                bind:value={config.ssh_config.key_path}
-              />
-            </div>
+            {#each Object.keys(config.ssh_config) as key}
+              <DynamicField config={config.ssh_config} configKey={key} />
+            {/each}
           </div>
         {:else if activeTab === "hugo"}
           <div class="space-y-4">
-            <!-- HUGO 설정 필드 -->
-            <div class="flex items-center space-x-2">
-              <label class="block min-w-[120px]" for="hugo_config-hugo_cmd_path"
-                >Hugo Cmd Path</label
-              >
-              <input
-                class="flex-1 p-2 border rounded"
-                id="hugo_config-hugo_cmd_path"
-                bind:value={config.cms_config.hugo_config.hugo_cmd_path}
-              />
-            </div>
-            <div class="flex items-center space-x-2">
-              <label class="block min-w-[120px]" for="hugo_config-base_path"
-                >Base Path</label
-              >
-              <input
-                class="flex-1 p-2 border rounded"
-                id="hugo_config-base_path"
-                bind:value={config.cms_config.hugo_config.base_path}
-              />
-            </div>
-            <div class="flex items-center space-x-2">
-              <label class="block min-w-[120px]" for="hugo_config-content_path"
-                >Content Path</label
-              >
-              <input
-                class="flex-1 p-2 border rounded"
-                id="hugo_config-content_path"
-                bind:value={config.cms_config.hugo_config.content_path}
-              />
-            </div>
-            <div class="flex items-center space-x-2">
-              <label class="block min-w-[120px]" for="hugo_config-image_path"
-                >Image Path</label
-              >
-              <input
-                class="flex-1 p-2 border rounded"
-                id="hugo_config-image_path"
-                bind:value={config.cms_config.hugo_config.image_path}
-              />
-            </div>
-            <div class="flex items-center space-x-2">
-              <label class="block min-w-[120px]" for="hugo_config-config_path"
-                >Config Path</label
-              >
-              <input
-                class="flex-1 p-2 border rounded"
-                id="hugo_config-config_path"
-                bind:value={config.cms_config.hugo_config.config_path}
-              />
-            </div>
-            <div class="flex items-center space-x-2">
-              <label class="block min-w-[120px]" for="hugo_config-layout_path"
-                >Layout Path</label
-              >
-              <input
-                class="flex-1 p-2 border rounded"
-                id="hugo_config-layout_path"
-                bind:value={config.cms_config.hugo_config.layout_path}
-              />
-            </div>
-            <div class="flex items-center space-x-2">
-              <label class="block min-w-[120px]" for="hugo_config-trashcan_path"
-                >Trashcan Path</label
-              >
-              <input
-                class="flex-1 p-2 border rounded"
-                id="hugo_config-trashcan_path"
-                bind:value={config.cms_config.hugo_config.trashcan_path}
-              />
-            </div>
+            {#each Object.keys(config.cms_config.hugo_config) as key}
+              <DynamicField config={config.cms_config.hugo_config} configKey={key} />
+            {/each}
           </div>
         {/if}
 
@@ -268,17 +137,6 @@
   }
 
   .tab-button.bg-active {
-    background-color: var(--button-hover-bg-color);
-  }
-
-  .password-toggle {
-    background-color: var(--button-bg-color);
-    color: var(--button-text-color);
-    cursor: pointer;
-    transition: background-color 0.25s;
-  }
-
-  .password-toggle:hover {
     background-color: var(--button-hover-bg-color);
   }
 
